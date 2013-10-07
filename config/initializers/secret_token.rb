d0cf605da2694e39484d2b7f09bfd69274e9587e
Metrocom Global Solusi <rengga.patria@metrocom.co.id>
2013-10-07 23:51:26 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '9c19eb3dac99d7efb6187fe0dc18e733c026971d09d810301779507656127f1e62dcca2780cca88d3bb53bcb2e4736cb2aa5879280167b2f88c2bbf3be9ab96d'
